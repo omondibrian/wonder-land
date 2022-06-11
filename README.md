02533c6d0b37ac9e6668471098c664cee47bb6c6
Felix Orinda <forinda82@gmail.com>
2022-06-11 15:37:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# A basic template for react developer with ts
+- Typescript
+- ReduxJs/toolkit
+- axios
+- Sass
+- Material UI
+- Material Icons
+- Recharts
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Graph from "components/Graph";
+import Table from "components/Table";
+import UserState from "components/UserState";
+import SocialMedia from "components/SocialMedia";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Wonderland</h1>
+    </div>
+  );
+}
+
+export default App;
