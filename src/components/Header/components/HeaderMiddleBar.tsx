5adc6f5e43ab503c7a70ab037b200b1a1871b1a2
Felix Orinda <forinda82@gmail.com>
2022-06-12 20:24:48 +0300

    Update header

--- src/components/Header/components/HeaderMiddleBar.tsx
+++ src/components/Header/components/HeaderMiddleBar.tsx
@@ -1,15 +1,68 @@
-import { Dehaze, Search, ShoppingBasket } from '@mui/icons-material';
+import { Close, Dehaze, Search, ShoppingBasket } from '@mui/icons-material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
+const sideLinks = [
+	{ title: 'Home', to: '/' },
+	{ title: 'Shop', to: '/shop' },
+	{ title: 'About', to: '/about' },
+	{ title: 'Contact', to: '/contact' },
+];
 const HeaderMiddleBar = () => {
 	const navigate = useNavigate();
+	const [sidebarOpen, setSidebarOpen] = React.useState<boolean>(false);
 	return (
 		<div className='flex order-1 md:order-2 justify-around md:justify-between px-8 md:px-20 py-2  md:py-10 bg-green-400 w-full'>
 			{/* Clickable  */}
-			<div className='flex align-middle justify-center md:hidden'>
+			<div
+				className='flex align-middle justify-center md:hidden'
+				onClick={(e) => {
+					setSidebarOpen(true);
+				}}
+			>
 				<Dehaze />
+			</div>
+			<div
+				className={
+					sidebarOpen
+						? 'fixed z-10 left-0 top-0 flex flex-col md:hidden h-screen w-screen bg-slate-800 bg-opacity-50'
+						: 'hidden fixed z-10 left-0 top-0  flex-col md:hidden h-screen w-screen bg-slate-800 bg-opacity-50'
+				}
+			>
+				<div className='p-2 bg-slate-50 h-full w-60'>
+					<button className='absolute top-2 right-4 bg-white border-none cursor-pointer '>
+						<Close
+							fontSize='large'
+							onClick={(e) => {
+								setSidebarOpen(false);
+							}}
+						/>
+					</button>
+					<div className='mb-4'>
+						<select name='' id=''>
+							<option value='some'>Some</option>
+							<option value='some'>Some</option>
+							<option value='some'>Some</option>
+							<option value='some'>Some</option>
+						</select>
+					</div>
+					<hr />
+					<div>
+						<ul className='flex flex-col gap-2'>
+							{sideLinks.map(({ title, to }) => (
+								<li key={title} className='flex align-middle'>
+									<Link
+										to={to}
+										className='px-4 w-full hover:bg-slate-400  uppercase font-medium text-md'
+									>
+										{title}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
 			</div>
 			{/* Logo */}
 			<div className='flex align-middle justify-center cursor-pointer'>
